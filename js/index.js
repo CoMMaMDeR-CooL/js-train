19114d9f644bfd5aa0e9e564758d71de31685d98
Dima <78321515+CommanderCoolDev@users.noreply.github.com>
2022-05-03 00:17:39 +0300

    testing

--- js/index.js
+++ js/index.js
@@ -1,4 +1,44 @@
 'use strict';
+
+// const burger = true;
+// const fries = true;
+
+// if (burger && fries) {
+//     console.log("gooood")
+// }
+// console.log(burger && fries)
+// const burger = 3;
+// const fries = 1;
+// const cola = 1;
+
+// console.log(burger === 3 && cola  && fries)
+
+
+// if (burger === 3 && cola  && fries ) {
+//     console.log("gooood x 2")
+// } else {
+//     console.log('moove along')
+//   }
+const burger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+
+
+
+
+if (burger ===3 && cola === 2   || fries === 3 && nuggets) {
+    console.log("gooood x 3")
+} else {
+    console.log('moove along')
+}
+
+
+// let johnReport, samReport, alexReport = 'done';
+// console.log(johnReport||samReport|| alexReport)
+
+
 // const secretNumber = Math.round(Math.random() * 100 + 1);
 // // console.log('secretNumber', secretNumber);
 // let userNumber;
